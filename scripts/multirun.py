fcaf0f0e81f92c5d6324a4de05196814a2a04abd
Abdurrahman Ozurk <aozturk@tamu.edu>
2020-05-13 16:01:14 +0300

    case6_hightemp_lowsink

--- scripts/multirun.py
+++ scripts/multirun.py
@@ -24,6 +24,4 @@ file = open(filename)
 csvfile = file.readlines()
 file.close()
 for i in range(0,len(csvfile)):
-    #directory = csvfile[i].split('.')[-2]
-    os.system("mkdir "+str(i))
-    os.system("mpiexec -n "+cores+" ~/projects/beaver/beaver-opt -i "+str(i)+"/"+csvfile[i])
+    os.system("mpiexec -n "+cores+" ~/projects/beaver/beaver-opt -i "+csvfile[i])
